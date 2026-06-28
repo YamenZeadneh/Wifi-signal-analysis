1dc38fe8460e07beb9d04ed8dcf73a81d75e6550
YamenZeadneh <yamenzeadneh@gmail.com>
2026-06-28 05:56:15 +0300

    _

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int sum(int a , int b){
+    return a + b;
+}
+
+int main(){
+    return 0;
+}
---
+++ test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+a = np.array([2, 1, 5, 7, 4, 6, 8, 14, 10, 9, 18, 20, 22])
+import matplotlib.pyplot as plt
+plt.plot(a)
